3b542791e3ad3c0f4d29a154eed041f7741e6618
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-11-17 07:30:19 +0100

    Get rid of tolerance in the triangle domain class

--- TypeOfShape.Domain/Triangle/Triangle.cs
+++ TypeOfShape.Domain/Triangle/Triangle.cs
@@ -6,60 +6,60 @@ namespace TypeOfShape.Domain.Triangle;
 public class Triangle : IShape<TriangleTypes>
 {
     private readonly double[] _sides;
-    private readonly double _tolerance;
 
     private Triangle(double[] sides)
     {
         _sides = sides;
-        _tolerance = CalculateTolerance(_sides);
     }
 
     public new TriangleTypes GetType()
     {
-        if (Math.Abs(_sides[0] - _sides[1]) < _tolerance
-            && Math.Abs(_sides[1] - _sides[2]) < _tolerance)
-            return TriangleTypes.Equilateral;
+        // a triangle is scalene if all sides are different
+        if (_sides[0] < _sides[1] && _sides[1] < _sides[2])
+        {
+            return TriangleTypes.Scalene;
+        }
 
-        if (Math.Abs(_sides[0] - _sides[1]) < _tolerance
-            || Math.Abs(_sides[1] - _sides[2]) < _tolerance
-            || Math.Abs(_sides[2] - _sides[0]) < _tolerance)
+        // a triangle is isosceles if two sides are equal
+        if (!(_sides[0] < _sides[1] && _sides[0] > _sides[1]) && _sides[1] < _sides[2]
+            || !(_sides[1] < _sides[2] && _sides[1] > _sides[2]) && _sides[0] < _sides[1]
+            || !(_sides[0] < _sides[2] && _sides[0] > _sides[2]) && _sides[1] < _sides[2])
+        {
             return TriangleTypes.Isosceles;
+        }
 
-        return TriangleTypes.Scalene;
+        // a triangle is equilateral if all sides are equal
+        return TriangleTypes.Equilateral;
     }
 
     public static ErrorOr<Triangle> CreateFromSides(double[] sides)
     {
-        if (sides.Length != 3) return CommonErrors.ToFewSidesError;
+        // a triangle is invalid if it does not have 3 sides
+        if (sides.Length != 3)
+        {
+            return CommonErrors.ToFewSidesError;
+        }
 
-        if (sides.Any(s => s <= 0)) return CommonErrors.ZeroOrNegativeSideError;
+        // a triangle is invalid if any of the sides are less than or equal to zero
+        if (sides.Any(s => s <= 0))
+        {
+            return CommonErrors.ZeroOrNegativeSideError;
+        }
 
         var sortedSides = sides.OrderBy(s => s).ToArray();
 
-        if (sortedSides[0] + sortedSides[1] < sortedSides[2]) return TriangleErrors.InvalidTriangleError;
+        // a triangle is invalid if the sum of two sides is less than the third side
+        if (sortedSides[0] + sortedSides[1] < sortedSides[2])
+        {
+            return TriangleErrors.InvalidTriangleError;
+        }
 
-        var tolerance = CalculateTolerance(sortedSides);
-
-        if (Math.Abs(sortedSides[0] + sortedSides[1] - sortedSides[2]) < tolerance)
+        // a triangle is flat if the sum of two sides is equal to the third side
+        if (!(sortedSides[0] + sortedSides[1] > sortedSides[2]))
+        {
             return TriangleErrors.FlatTriangleError;
-
+        }
+        
         return new Triangle(sortedSides);
-    }
-
-    private static double CalculateTolerance(IReadOnlyList<double> sides)
-    {
-        const double tolerance = 0.001;
-
-        var min = sides.Min();
-        var middle = sides[1];
-        var max = sides.Max();
-
-        if (min < tolerance) return tolerance * min;
-
-        if (max - min < tolerance && max > min) return tolerance * (max - min);
-        if (middle - min < tolerance && middle > min) return tolerance * (middle - min);
-        if (max - middle < tolerance && max > middle) return tolerance * (max - middle);
-
-        return tolerance;
     }
 }
